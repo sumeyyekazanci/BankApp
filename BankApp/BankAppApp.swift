0ca85de3fa7c96f3b501f13306299ab69c4e22e7
Sumeyye Kazanci <sumeyyekazanci@icloud.com>
2022-04-27 15:15:08 +0300

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Sümeyye Kazancı on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
